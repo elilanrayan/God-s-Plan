5a4e4b53b4fe09a4fab299ebf75fb3c990d70816
elilanrayan <rayan2005@outlook.fr>
2024-10-06 17:16:10 +0200

    [ADD]

---
+++ Assets/TilePop.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NaughtyAttributes;
+using DG.Tweening;
+using TMPro;
+using UnityEngine.UI;
+
+public class TilePop : MonoBehaviour
+{
+   
+    [SerializeField] private TMP_Text popUp_TXT;
+   
+}
