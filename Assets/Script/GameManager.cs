29c957858bab374120c61c96f7bc672c4f66843f
GuyomeC <146094089+GuyomeC@users.noreply.github.com>
2024-10-07 11:07:47 +0200

    [ADD] Image + [UPDATE] ChoiceButton

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -4,12 +4,30 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-
 public class GameManager : MonoBehaviour
 {
+    public static GameManager Instance { get; private set; }
+
+
     private ChoicesDisplay choicesDisplay;
     [SerializeField] private Button species1;
     [SerializeField] private Button species2;
+
+    private List<Choices> finalSpecies;
+
+    private int laps = 0;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
 
     void Start()
     {
@@ -21,14 +39,58 @@ public class GameManager : MonoBehaviour
 
     void Update()
     {
-        
+        if(laps >= 15)
+        {
+            //Active ecran fin
+        }
     }
 
-    public void MancheSuivante()
+    public void MancheSuivante1()
     {
-        choicesDisplay.Firstspecies = choicesDisplay.Species[2];
-        choicesDisplay.Secondspecies = choicesDisplay.Species[3];
+        // Enlever les espèces affichées précédemment par indice
+        int indexFirst = choicesDisplay.Species.IndexOf(choicesDisplay.Firstspecies);
+        int indexSecond = choicesDisplay.Species.IndexOf(choicesDisplay.Secondspecies);
+        finalSpecies.Add(choicesDisplay.Firstspecies);
+
+        if (indexFirst > indexSecond)
+        {
+            choicesDisplay.Species.RemoveAt(indexFirst);
+            choicesDisplay.Species.RemoveAt(indexSecond);
+        }
+        else
+        {
+            choicesDisplay.Species.RemoveAt(indexSecond);
+            choicesDisplay.Species.RemoveAt(indexFirst);
+        }
+
+        choicesDisplay.Firstspecies = choicesDisplay.Species[4];
+        choicesDisplay.Secondspecies = choicesDisplay.Species[5];
         choicesDisplay.UpdateDisplay();
+        laps += 1;
+    }
+    
+    public void MancheSuivante2()
+    {
+        // Enlever les espèces affichées précédemment par indice
+        int indexFirst = choicesDisplay.Species.IndexOf(choicesDisplay.Firstspecies);
+        int indexSecond = choicesDisplay.Species.IndexOf(choicesDisplay.Secondspecies);
+        finalSpecies.Add(choicesDisplay.Secondspecies);
+
+        if (indexFirst > indexSecond)
+        {
+            choicesDisplay.Species.RemoveAt(indexFirst);
+            choicesDisplay.Species.RemoveAt(indexSecond);
+        }
+        else
+        {
+            choicesDisplay.Species.RemoveAt(indexSecond);
+            choicesDisplay.Species.RemoveAt(indexFirst);
+        }
+
+        choicesDisplay.Firstspecies = choicesDisplay.Species[4];
+        choicesDisplay.Secondspecies = choicesDisplay.Species[5];
+        choicesDisplay.UpdateDisplay();
+        laps += 1;
     }
 
 }
